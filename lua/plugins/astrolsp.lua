719761f42e0dd6df242f6b5c8a9d3a9d130c2a57
Elijah Smith <esmith@jetblue.com>
2026-03-12 16:09:16 -0500

    enable inlay hints and disable vtsls autoformatting in favor of prettier

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -13,7 +13,7 @@ return {
     -- Configuration table of features provided by AstroLSP
     features = {
       codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
+      inlay_hints = true, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
     },
     -- customize lsp formatting options
@@ -31,6 +31,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "vtsls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
